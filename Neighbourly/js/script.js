ada599a6cfbc01182b1267086ca15a6c903fa42f
otakuchan <ianbjs@gmail.com>
2022-02-23 19:05:49 -0800

    feat: initial signup/login/logout

---
+++ Neighbourly/js/script.js
@@ -0,0 +1,23 @@
+
+let userSignup = [];
+
+// class User {
+//     constructor(postalCode, address, email) {
+//         this.postalCode = postalCode;
+//         this.address = address;
+//         this.email = email;
+//     }
+// }
+
+// const signupHome = document.getElementById("signupHome");
+
+// signupHome.addEventListener("submit", function (event) {
+//     event.preventDefault();
+//     const postalInput = signupHome.querySelector("#postalCode");
+//     const address = signupHome.querySelector("#address");
+//     const email = signupHome.querySelector("#email");
+
+//     const user = new User(postalInput.value, address.value, email.value);
+
+//     userSignup.push(user);
+// });
