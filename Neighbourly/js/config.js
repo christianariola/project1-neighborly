c3864ef97403a2a2e78e92fedd1daec1ae598b93
otakuchan <ianbjs@gmail.com>
2022-02-24 13:28:41 -0800

    feat: added index, signup, login and logout

---
+++ Neighbourly/js/config.js
@@ -0,0 +1,16 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBB9Jml1C-kCtAJZ0PzHmJL6-fobbJdig4",
+  authDomain: "team7-8fad9.firebaseapp.com",
+  projectId: "team7-8fad9",
+  storageBucket: "team7-8fad9.appspot.com",
+  messagingSenderId: "1004780029762",
+  appId: "1:1004780029762:web:520749c7c8301d6c1faadb",
+  measurementId: "G-GWTDFWLMX0"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// make auth and firestore references
+const auth = firebase.auth();
+const db = firebase.firestore();
