711eb6dec95fa22cfadb1e14816f053ebf666364
otakuchan <ianbjs@gmail.com>
2022-03-08 10:08:08 -0800

    feat: google autocomplete address form

--- Neighbourly/js/auth_signup.js
+++ Neighbourly/js/auth_signup.js
@@ -2,18 +2,98 @@
 let sessionString = sessionStorage.getItem('UserInfoHome')
 let userSignup = JSON.parse(sessionString);
 
+let autocomplete;
+let address1Field;
+let postalField;
+
+function initAutocomplete() {
+    address1Field = document.querySelector("#ship-address");
+    postalField = document.querySelector("#postcode");
+    // Create the autocomplete object, restricting the search predictions to
+    // addresses in the US and Canada.
+    autocomplete = new google.maps.places.Autocomplete(address1Field, {
+        componentRestrictions: { country: ["ca"] },
+        fields: ["address_components", "geometry"],
+        types: ["address"],
+    });
+    address1Field.focus();
+    // When the user selects an address from the drop-down, populate the
+    // address fields in the form.
+
+    autocomplete.addListener("place_changed", fillInAddress);
+}
+
+function fillInAddress() {
+    // Get the place details from the autocomplete object.
+    const place = autocomplete.getPlace();
+    let address1 = "";
+    let postcode = "";
+
+    // Get each component of the address from the place details,
+    // and then fill-in the corresponding field on the form.
+    // place.address_components are google.maps.GeocoderAddressComponent objects
+    // which are documented at http://goo.gle/3l5i5Mr
+    for (const component of place.address_components) {
+    const componentType = component.types[0];
+
+    switch (componentType) {
+        case "street_number": {
+        address1 = `${component.long_name} ${address1}`;
+        break;
+        }
+
+        case "route": {
+        address1 += component.short_name;
+        break;
+        }
+
+        case "postal_code": {
+        postcode = `${component.long_name}${postcode}`;
+        break;
+        }
+
+        case "postal_code_suffix": {
+        postcode = `${postcode}-${component.long_name}`;
+        break;
+        }
+        case "locality":
+        document.querySelector("#locality").value = component.long_name;
+        break;
+        case "administrative_area_level_1": {
+        document.querySelector("#state").value = component.short_name;
+        break;
+        }
+        case "country":
+        document.querySelector("#country").value = component.long_name;
+        break;
+    }
+    }
+
+    address1Field.value = address1;
+    postalField.value = postcode;
+
+    // get lat
+    var lat = place.geometry.location.lat();
+    // get lng
+    var lng = place.geometry.location.lng();
+
+    console.log("Latitude: "+lat+" - "+"Longitude: "+lng);
+}
+
 const signupBtn = document.getElementById("signupBtn");
 
 signupBtn.addEventListener("click", (event) => {
     event.preventDefault();
-    const phonenumber = document.getElementById("phone_number").value;
-    const password = document.getElementById("password").value;
 
     const firstName = userSignup[0].firstName;
     const lastName = userSignup[0].lastName;
-    const postalCode = userSignup[0].postalCode;
-    const address = userSignup[0].address;
     const email = userSignup[0].email;
+    const password = userSignup[0].password;
+    const phonenumber = userSignup[0].phonenumber;
+
+    const locality = document.querySelector("#locality");
+    const state = document.querySelector("#state");
+    const country = document.querySelector("#country");
  
     db.collection("users").where("email", "==", email)
     .get().then((querySnapshot) => {
@@ -40,9 +120,9 @@ signupBtn.addEventListener("click", (event) => {
             email: email,
             password: password,
             phonenumber: phonenumber,
-            postalCode: postalCode,
-            address: address,
-            created_at: new Date()
+            // address: { address1: address1Field, locality: locality, state: state, country: country, postcode: postalField },
+            // location: { latitude: lat, longitude: lng },
+            // created_at: new Date(),
         }).then(() => {
             // remove array from first page
             sessionStorage.removeItem("UserInfoHome");
