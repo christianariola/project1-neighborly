42ddb71f193524381bf994d8c5595602e4d9fc08
Karan <ksingh090@mylangara.ca>
2022-03-28 04:34:38 -0700

    Multi category Filters

--- Neighbourly/js/filters.js
+++ Neighbourly/js/filters.js
@@ -20,13 +20,6 @@ const baby_and_kids = document.getElementById("baby_and_kids");
 const appliances = document.getElementById("appliances");
 const helprequest_type = document.getElementById("helprequest_type");
 
-function onlyOne(checkbox) {
-    var checkboxes = document.getElementsByName('category')
-    checkboxes.forEach((item) => {
-        if (item !== checkbox) item.checked = false
-    })
-}
-
 let stopListineng;
 async function onloadsetshow(){
     filterSelection("all");
@@ -49,26 +42,6 @@ async function filterSelection(c) {
 }
 onloadsetshow();
 
-function filterhelptype(c) {
-    if (stopListineng) stopListineng();
-    const postCollection = c
-
-    // const postsCollection = c == '' ? dbCollection("posts").where("compensation", "==", c && "type", "==", "help_request") : dbCollection("posts", 'compensation').orderBy('createdAt').where("compensation", "!=", c && "type", "==", "help_request");
-
-    if (c==''){
-        const postsCollection = dbCollection("posts").where("compensation", "==", c && "type", "==", "help_request");
-        stopListineng = postsCollection.onSnapshot(async (querySnapshot) => {
-            await populateFeed(querySnapshot);
-        });
-    }
-    else{
-        const postsCollection = dbCollectioncomp("posts").where("compensation", "!=", '' && "type", "==", "help_request")
-        stopListineng = postsCollection.onSnapshot(async (querySnapshot) => {
-            await populateFeed(querySnapshot);
-        });
-    }
-}
-
 const btn = document.querySelector('#category_btn');
 btn.addEventListener('click', (event) => {
     resetFeed();
@@ -80,6 +53,21 @@ btn.addEventListener('click', (event) => {
     });
 
     let Compcheckboxes = document.querySelectorAll('input[name="compensation"]:checked');
+    console.log(Compcheckboxes.length);
+    if (Compcheckboxes.length == 0 ||checkboxes.length == 0) {
+        Toastify({
+            text: "Please select compensation and Category",
+            duration: 2000,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: 'right', // `left`, `center` or `right`
+            backgroundColor: "linear-gradient(to right, #00b09b, #70C782)",
+        }).showToast();
+        setTimeout(() => {
+        window.location.reload();
+        }, 1000);
+        return;
+    }
     console.log(Compcheckboxes[0].value);
     let c = Compcheckboxes[0].value;
     // Compcheckboxes.forEach((c) => {
@@ -102,30 +90,80 @@ btn.addEventListener('click', (event) => {
 });
 
 
+////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+
+const givebtn = document.querySelector('#GiveCategory_btn');
+givebtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    resetFeed();
+    let givecheckboxes = document.querySelectorAll('input[name="GiveawayCategory"]:checked');
+    let values = [];
+    givecheckboxes.forEach((checkbox) => {
+        values.push(checkbox.value);
+    });
+
+    if (values.length > 10) {
+        Toastify({
+            text: "Please select less than 10 categories",
+            duration: 2000,
+            close: true,
+            gravity: "bottom", // `top` or `bottom`
+            position: 'center', // `left`, `center` or `right`
+            backgroundColor: "linear-gradient(to right, #00b09b, #70C782)",
+        }).showToast();
+        setTimeout(() => {
+            window.location.reload();
+        }, 1000);
+        return;
+    }
+
+    let Condcheckboxes = document.querySelectorAll('input[name="condition"]:checked');
+    console.log(Condcheckboxes.length);
+    if (Condcheckboxes.length == 0 || givecheckboxes.length == 0) {
+        Toastify({
+            text: "Please select compensation and Category",
+            duration: 2000,
+            close: true,
+            gravity: "bottom", // `top` or `bottom`
+            position: 'center', // `left`, `center` or `right`
+            backgroundColor: "linear-gradient(to right, #00b09b, #70C782)",
+        }).showToast();
+        setTimeout(() => {
+            window.location.reload();
+        }, 1000);
+        return;
+    }
+    console.log(Condcheckboxes[0].value);
+    let c = Condcheckboxes[0].value;
+    // Compcheckboxes.forEach((c) => {
+    //     values.push(c.value);
+    // });
+    // const c = document.getElementById("helprequest_type");
+    // console.log(c.value);
+    if (stopListineng) stopListineng();
+    if (c == '') {
+        const postsCollection = dbCollectioncomp("posts").where("condition", "==", c && "type", "==", "giveaway").where("category", "in", values);
+        stopListineng = postsCollection.onSnapshot(async (querySnapshot) => {
+            await populateFeed(querySnapshot);
+        });
+    } else {
+        const postsCollection = dbCollectioncomp("posts").where("condition", "!=", '' && "type", "==", "giveaway").where("category", "in", values);
+        stopListineng = postsCollection.onSnapshot(async (querySnapshot) => {
+            await populateFeed(querySnapshot);
+        });
+    }
+});
 
 
-//     const postsCollection = values.length == 0 ? dbCollection("posts") : dbCollection("posts").where("category", "==", values[0] || "category", "==", values[1] || "category", "==", values[2] || "category", "==", values[3] || "category", "==", values[4] || "category", "==", values[5] || "category", "==", values[6] || "category", "==", values[7] || "category", "==", values[8] || "category", "==", values[9] || "category" || "category", "==", values[10] || "category" || "category", "==", values[11] || "category" || "category", "==", values[12] || "category" || "category", "==", values[13] || "category" || "category", "==", values[14] || "category" || "category", "==", values[15] || "category" || "category", "==", values[16] || "category" || "category", "==", values[17] || "category" || "category", "==", values[18] || "category" || "category", "==", values[19] || "category" || "category", "==", values[20] || "category" || "category", "==", values[21] || "category" || "category", "==", values[22] || "category" || "category", "==", values[23] || "category" || "category", "==", values[24] || "category" || "category", "==", values[25] || "category" || "category", "==", values[26] || "category" || "category", "==", values[27] || "category" || "category", "==", values[28] || "category" || "category", "==", values[29] || "category" || "category", "==", values[30] || "category" || "category", "==", values[31] || "category" || "category", "==", values[32] || "category" || "category", "==", values[33] || "category" || "category", "==", values[34] || "category" || "category", "==", values[35] || "category" || "category", "==", values[36] || "category" || "category", "==", values[37] || "category" || "category", "==", values[38] || "category" || "category", "==", values[39] || "category" || "category", "==", values[40] || "category" || "category", "==", values[41] || "category" || "category", "==", values[42] || "category" || "category", "==", values[43] || "category" || "category").where("type", "==", "help_request");
-//     stopListineng = postsCollection.onSnapshot(async (querySnapshot) => {
-//         await populateFeed(querySnapshot);
-//     });
-// });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// function onlyOne(checkbox) {
+//     var checkboxes = document.getElementsByName('category')
+//     checkboxes.forEach((item) => {
+//         if (item !== checkbox) item.checked = false
+//     })
+// }
 
 function onlytwo(checkbox) {
     var checkboxes = document.getElementsByName('compensation')
@@ -133,3 +171,15 @@ function onlytwo(checkbox) {
         if (item !== checkbox) item.checked = false
     })
 }
+function onlythree(checkbox) {
+    var checkboxes = document.getElementsByName('condition')
+    checkboxes.forEach((item) => {
+        if (item !== checkbox) item.checked = false
+    })
+}
+// function onlyfour(checkbox) {
+//     var checkboxes = document.getElementsByName('GiveawayCategory')
+//     checkboxes.forEach((item) => {
+//         if (item !== checkbox) item.checked = false
+//     })
+// }
