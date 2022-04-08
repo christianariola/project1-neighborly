73a64658be37152453114eed2fc646cfd486ef0e
otakuchan <ianbjs@gmail.com>
2022-04-07 22:08:54 -0700

    fixed: offline message on mobile

--- Neighbourly/js/dashboard/main.js
+++ Neighbourly/js/dashboard/main.js
@@ -91,8 +91,6 @@ class Dashboard {
                     zoomLvl = zoomLvl;
                 }
 
-                //console.log(rad);
-
                 //get list of users with the same locality and state
                 const usersCollectionRef = db.collection('users');
                 // const usersQuery = usersCollectionRef.where('address.locality', '==', snapshot.data().address.locality).where('userId', '!=', userid);
@@ -101,7 +99,6 @@ class Dashboard {
 
                 usersSnapshot.forEach(doc => {
                     uids.push(doc.data());
-                    //console.log("ADDRESS: "+doc.data().userId);
                 });
 
                 const postsRef = db.collection('posts');
@@ -116,8 +113,6 @@ class Dashboard {
 
 
                     if(checker) {
-
-
                         let postRef = postsRef.where('author', '==', uid.userId);
                         let postSnapshot = await postRef.get();
 
@@ -205,33 +200,17 @@ slider.oninput = function() {
         zoomLvl = 12;
     }
 
-    //console.log("TEST "+rad);
-
     const dashboard2 = new Dashboard;
     dashboard2.getUser(userIdent, rad, zoomLvl);
 
-    //initMap(ltude, lngtude, rad, zoomLvl);
-
     outputKM.innerHTML = this.value;
 }
-
-// Calculation if coords is inside radius
-// function arePointsNear(marker, circle, radius) {
-//     var km = radius/1000;
-//     var kx = Math.cos(Math.PI * circle.lat / 180) * 111;
-//     var dx = Math.abs(circle.lng - marker.lng) * kx;
-//     var dy = Math.abs(circle.lat - marker.lat) * 111;
-//     return Math.sqrt(dx * dx + dy * dy) <= km;
-// }
 
 function initMap(latitude, longitude, rad, zoomLvl, nearList) {
 
     let lati = parseFloat(latitude);
     let long = parseFloat(longitude);
     let latlng = new google.maps.LatLng(lati, long);
-
-    // console.log("RAD: "+rad);
-    // console.log("ZOOM: "+zoomLvl);
 
     map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 49.22493043847068, lng: -123.10865688997087 },
@@ -300,8 +279,6 @@ function initMap(latitude, longitude, rad, zoomLvl, nearList) {
 
     // // Group post by author
     let nearbyPosts = nearList.reduce((r, a) => {
-        // console.log("a", a.postInfo.author);
-        // console.log('r', r);
         r[a.postInfo.author] = [...r[a.postInfo.author] || [], a];
         return r;
     }, {});
@@ -313,29 +290,18 @@ function initMap(latitude, longitude, rad, zoomLvl, nearList) {
                 userPost += '<div id="siteNotice">';
                 userPost += '<h4>'+ nearbyPosts[postskey][postkey].postInfo.title +'</h4>';
                 userPost += '<p class="post-createdAt">'+ dbTimestampToDate(nearbyPosts[postskey][postkey].postInfo.createdAt).toString().substring(0, 25) +'</p>';
-                // chantest += '<div id="bodyContent"><p class="post-popup-type">Type: <span>' + nearbyPosts[postskey][postkey].postInfo.type + '</span></p>';
                 userPost += '<div><a href="#'+nearbyPosts[postskey][postkey].postInfo.id+'">View Post</a></div></div>';
             }
-            // console.log("CHANS: "+chantest);
 
             let temppos = new google.maps.LatLng(nearbyPosts[postskey][0].location.lat, nearbyPosts[postskey][0].location.lng);
         
             const postContent =
             '<div id="content">' +
-            // '<div id="siteNotice">' +
             userPost +
-            // '<h4>'+ nearbyPosts[postskey][postkey].postInfo.title +'</h4>' +
-            // '<p class="post-createdAt">'+ dbTimestampToDate(nearbyPosts[postskey][postkey].postInfo.createdAt).toString().substring(0, 25) +'</p>' +
-            // '<div id="bodyContent">' +
-            // '<p class="post-popup-type">Type: <span>' + nearbyPosts[postskey][postkey].postInfo.type + '</span></p>' +
-            // '<div><a href="#!">View Post</a></div>' +
-            // "</div>" +
-            // "</div>" +
             "</div>";
 
             infowindows[cnt] = new google.maps.InfoWindow({
                 content: postContent,
-                //maxWidth: 200
             });
 
             postmarker = '../../Neighbourly/images/markers/multiple-icon.png';
@@ -356,7 +322,6 @@ function initMap(latitude, longitude, rad, zoomLvl, nearList) {
             bounds.extend(mark[cnt].getPosition());
 
         } else {
-            //console.log("equal 1");
             for (let singlepostkey in nearbyPosts[postskey]) {
                 let temppos = new google.maps.LatLng(nearbyPosts[postskey][singlepostkey].location.lat, nearbyPosts[postskey][singlepostkey].location.lng);
             
@@ -374,7 +339,6 @@ function initMap(latitude, longitude, rad, zoomLvl, nearList) {
 
                 infowindows[cnt] = new google.maps.InfoWindow({
                     content: postContent,
-                    //maxWidth: 200
                 });
 
                 switch(nearbyPosts[postskey][singlepostkey].postInfo.type) {
@@ -407,74 +371,10 @@ function initMap(latitude, longitude, rad, zoomLvl, nearList) {
                 bounds.extend(mark[cnt].getPosition());
             }
 
-
         }
 
         cnt++;
     }
-
-
-
-
-    // for (let key in nearList) {
-    //     // skip loop if the property is from prototype
-    //     //if(!obj.hasOwnProperty(prop)) continue;
-
-    //     let temppos = new google.maps.LatLng(nearList[key].location.lat, nearList[key].location.lng);
-
-    //     const postContent =
-    //     '<div id="content">' +
-    //     '<div id="siteNotice">' +
-    //     "</div>" +
-    //     '<h4>'+ nearList[key].postInfo.title +'</h4>' +
-    //     '<p class="post-createdAt">'+ dbTimestampToDate(nearList[key].postInfo.createdAt).toString().substring(0, 25) +'</p>' +
-    //     '<div id="bodyContent">' +
-    //     '<p>' + nearList[key].postInfo.description + '</p>' +
-    //     '<p class="post-popup-type">Type: <span>' + nearList[key].postInfo.type + '</span></p>' +
-    //     '<div><a href="#!">View Post</a></div>' +
-    //     "</div>" +
-    //     "</div>";
-
-    //     infowindows[cnt] = new google.maps.InfoWindow({
-    //         content: postContent,
-    //         //maxWidth: 200
-    //     });
-
-    //     switch(nearList[key].postInfo.type) {
-    //         case "recommendation":
-    //             postmarker = '../../Neighbourly/images/markers/recommendation-icon.png';
-    //         break;
-    //         case "help_request":
-    //             postmarker = '../../Neighbourly/images/markers/help-request-icon.png';
-    //         break;
-    //         case "giveaway":
-    //             postmarker = '../../Neighbourly/images/markers/giveaway-icon.png';
-    //         break;
-    //         default:
-    //             postmarker = 'http://www.google.com/intl/en_us/mapfiles/ms/micons/green-dot.png';
-    //     }
-
-    //     mark[cnt] = new google.maps.Marker({
-    //         position: temppos,
-    //         map: map,
-    //         animation:google.maps.Animation.DROP,
-    //         icon: postmarker
-    //     });
-
-    //     google.maps.event.addListener(mark[cnt], 'click', (function(markerrr, cnt) {
-    //         return function() {
-    //             infowindows[cnt].open(map, mark[cnt]);
-    //         }
-    //     })(mark[cnt], cnt));
-
-    //     bounds.extend(mark[cnt].getPosition());
-
-    //     cnt++;
-    // }
-
-    // marker.setIcon(image);
-    // infoWindow.setContent("Your Location");
-    // infoWindow.open(map);
 
     // END OF USER LOCATION MARKER AND INFO ==========================================
 
